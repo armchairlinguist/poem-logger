4637a2d2ce3e189efe849ce75681396d9d4bf99a
Alexis Grant <alexisg@gmail.com>
2017-10-25 14:30:44 -0700

    log to stdout if output is not a tty

--- poem_logger.rb
+++ poem_logger.rb
@@ -1,16 +1,23 @@
 require 'HTTParty'
 require 'remote_syslog_logger'
+require 'logger'
 require 'oga'
 
-rs_logger = RemoteSyslogLogger.new(ENV['LOG_HOST'], ENV['LOG_PORT'], program: 'poem-logger')
-
-# It's not important for poems to tell us when they happened :)
-rs_logger.formatter = proc do |severity, datetime, progname, msg|
-   "#{msg}\n"
-end
+$stdout.sync = true
 
 response = HTTParty.get('http://botpoet.com/random/vote/')
 document = Oga.parse_html(response.body)
 poem = document.css('pre.poem').text
 
-rs_logger.info(poem)
+if STDOUT.isatty
+  logger = RemoteSyslogLogger.new(ENV['LOG_HOST'], ENV['LOG_PORT'], program: 'poem-logger')
+else
+  logger = Logger.new $stdout
+end
+
+# It's not important for poems to tell us when they happened :)
+logger.formatter = proc do |severity, datetime, progname, msg|
+   "#{msg}\n"
+end
+
+logger.info(poem)
